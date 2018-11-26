52368d81b37edb49ee9599525f4cb6b3405cb8d7
pashute <pashute@gmail.com>
2018-11-26 10:28:57 +0200

    not updating ui

--- app/main.js
+++ app/main.js
@@ -1,25 +1,22 @@
 /// tabanno main js file
 
 // thanks to https://gooroo.io/GoorooTHINK/Article/16413/Creating-a-Chrome-extension-using-Visual-Studio-Code/19953#.W_UJ5ugzaHt 
-console.log("hi");
 
-$(function () {
-    $("#maintxt").html("set!")
-    console.log('ready!');
+var con = chrome.extension.getBackgroundPage();
 
-});
+con.console.log("hi");
 
 // returns tabs array with tab.url tab.title
 var filter = {}; // json: active, etc. see https://developer.chrome.com/extensions/tabs
 
 function onReceived(tabs){
-    console.log("onReceived: ", tabs);
+    con.console.log("onReceived: ", tabs);
 }
 
 function getTabs(clbkOnReceived){
     chrome.tabs.query(  filter, 
                         fucnction onQryResult(tabs){ results callback
-                            console.log("onQ.tabs: ", tabs)
+                            con.console.log("onQ.tabs: ", tabs)
                             clbkOnReceived(tabs);
                         }
                      );
